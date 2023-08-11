39f145790e9180d482831e21965e012019d4709e
Lasse <lasseh0310@gmail.com>
2023-08-11 15:52:48 +0200

    update appendix replication bash

--- replicate_appendix.sh
+++ replicate_appendix.sh
@@ -8,3 +8,15 @@ echo "Running analyses for feature selection by model"
 python src/application/main_experiment/eval/feature_selection_by_model.py
 echo "Running analyses for causal discovery example"
 python src/application/main_experiment/eval/causal_discovery_example.py
+
+echo "Running analyses for distributions and statistical tests for main benchmark data"
+python src/application/main_experiment/eval/distributions_significance/main_distributions.py
+echo "Running analyses for distributions and statistical tests for noise data"
+python src/application/main_experiment/eval/distributions_significance/label_noise_distributions.py
+python src/application/main_experiment/eval/distributions_significance/label_noise_summary_table.py
+echo "Running analyses for postprocessing original data, main benchmark data"
+python src/application/main_experiment/eval/distributions_significance/org_data_postprocessing_main_experiment.py
+echo "Running analyses for postprocessing original data, label noise data"
+python src/application/main_experiment/eval/distributions_significance/org_data_postprocessing_noise.py
+echo "Running analyses for additional statistical fidelity metrics"
+python src/application/main_experiment/eval/distributions_significance/stat_fid_metrics.py
